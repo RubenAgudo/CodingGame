96e26e88afc7a79c934d02a58b7ed2eee6b2320f
Ruben Agudo <ruben.agudo@cern.ch>
2016-10-21 12:09:47 +0200

    Finished SkynetRevolutionEp1

--- SkynetRevolutionEp1/Main.java
+++ SkynetRevolutionEp1/Main.java
@@ -8,69 +8,77 @@ import java.math.*;
  **/
 class Player {
 
+    private static Map<Integer, Set<Integer>> graph;
+
     public static void main(String args[]) {
         Scanner in = new Scanner(System.in);
         int N = in.nextInt(); // the total number of nodes in the level, including the gateways
         int L = in.nextInt(); // the number of links
         int E = in.nextInt(); // the number of exit gateways
 
-        Map<Integer, Set<Integer>> graph = new HashMap<Integer, Set<Integer>>();
-        Map<Integer, Integer> originalLinks = new HashMap<Integer, Integer>();
+        graph = new HashMap<Integer, Set<Integer>>();
+
+        Set<Integer> gateways = new TreeSet<Integer>();
+
         for (int i = 0; i < L; i++) {
             int N1 = in.nextInt(); // N1 and N2 defines a link between these nodes
             int N2 = in.nextInt();
-
-            originalLinks.put(N1, N2);
 
             addNode(N1, N2);
             addNode(N2, N1);
         }
         for (int i = 0; i < E; i++) {
             int EI = in.nextInt(); // the index of a gateway node
+            gateways.add(EI);
         }
 
         // game loop
         while (true) {
             int SI = in.nextInt(); // The index of the node on which the Skynet agent is positioned this turn
 
-            if(areConnected(EI, SI)) { //We delete the link that connects the gateway and skynet
-                severeLink(EI, SI);
-                severeLink(SI, EI);
+            Set<Integer> intersection = new TreeSet<Integer>(graph.get(SI));
+            intersection.retainAll(gateways);
+            
+            if(!intersection.isEmpty()) { //it will always have just one element
+                Iterator<Integer> iterator = intersection.iterator();
+                Integer gatewayToProtect = iterator.next();
 
-                Integer y = originalLinks.get(EI);
+                severeLink(gatewayToProtect, SI);
+                severeLink(SI, gatewayToProtect);
 
-                if(y == null) {
-                    System.out.println(EI + " " + y);
-                } else {
-                    System.out.println(y + " " + EI);
-                }
-
+                System.out.println(gatewayToProtect + " " + SI);
             } else { //We just delete one link
-                Set<Integer> links = graph.get(EI);
-                Iterator<Integer> iterator = links.iterator();
-                if(iterator.hasNext()) {
-                    Integer removed = iterator.next().remove();
-                    Integer y = originalLinks.get(EI);
-                    System.out.println(y + " " + EI);
+                boolean removedNode = false;
+
+                Iterator<Integer> gatewaysIterator = gateways.iterator();
+
+                while(!removedNode && gatewaysIterator.hasNext()) {
+                    Integer EI = gatewaysIterator.next();
+
+                    Set<Integer> links = graph.get(EI);
+                    Iterator<Integer> iterator = links.iterator();
+                    if(!removedNode && iterator.hasNext()) {
+                        removedNode = true;
+                        Integer removed = iterator.next();
+                        iterator.remove();
+                        System.out.println(removed + " " + EI);
+                    }
                 }
             }
-
-
-            // Example: 0 1 are the indices of the nodes you wish to sever the link between
-            System.out.println("0 1");
         }
     }
 
     static void addNode(Integer source, Integer target) {
-        List<Integer> nodes = graph.get(source);
+        Set<Integer> nodes = graph.get(source);
         if(nodes == null) {
             nodes = new TreeSet<Integer>();
         }
-        graph.put(source, nodes.add(target));
+        nodes.add(target);
+        graph.put(source, nodes);
     }
 
     static boolean areConnected(Integer source, Integer target) {
-        List<Integer> nodes = graph.get(source);
+        Set<Integer> nodes = graph.get(source);
         if(nodes == null) {
             nodes = new TreeSet<Integer>();
         }
@@ -78,7 +86,7 @@ class Player {
     }
 
     static boolean severeLink(Integer source, Integer target) {
-        List<Integer> nodes = graph.get(source);
+        Set<Integer> nodes = graph.get(source);
         if(nodes == null) {
             nodes = new TreeSet<Integer>();
         }
